d507567ce04667ad3b80a1d2ad32fd5db3dc0abf
autom8ter <colemanword@gmail.com>
2020-07-09 16:04:07 -0600

    test validation

--- render/render_test.go
+++ render/render_test.go
@@ -10,12 +10,15 @@ import (
 func TestRenderer(t *testing.T) {
 	renderer := render.NewRenderer()
 	if err := renderer.LoadSources(context.Background(), map[string]string{
-		"tmp": "https://raw.githubusercontent.com/autom8ter/getter-render/master/LICENSE",
+		"tmp": "https://raw.githubusercontent.com/autom8ter/getter-render/master/test.txt",
 	}); err != nil {
 		t.Fatal(err.Error())
 	}
 	for name, _ := range renderer.FileSet() {
 		t.Logf("filename: %s\n", name)
 	}
-	renderer.Compile(viper.AllSettings())
+	viper.Set("name", "Coleman Word")
+	if err := renderer.Compile(viper.AllSettings()); err != nil {
+		t.Fatal(err.Error())
+	}
 }
